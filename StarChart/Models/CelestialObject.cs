6e022ef18669ab3c05ae5bf69bb7356b25573eef
punniyakotti.mp <punniyakotti.mp@pg.com>
2022-08-09 19:03:46 +0530

    created CelestialObject model

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id{ get; set; }
+        [Required] public string Name{ get; set; }
+        public int? OrbitedObjectId { get; set; }
+        [NotMapped] public List<CelestialObject> Satellites { get; set;}
+        public System.TimeSpan OrbitalPeriod { get; set; }
+    }
+} 
