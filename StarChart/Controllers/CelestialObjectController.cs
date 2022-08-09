16ed97a3d303ea94b995c43d8bb374f6361da85a
punniyakotti.mp <punniyakotti.mp@pg.com>
2022-08-09 19:23:23 +0530

    created CelestialObjectController class

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    public class CelestialObjectController : ControllerBase
+    {
+    }
+}
--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -6,10 +6,11 @@ namespace StarChart.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<CelestialObject> CelestialObjects { get; set; }
+        private readonly ApplicationDbContext _context;
 
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-            
+            _context = new ApplicationDbContext(options);
         }
     }
 }
